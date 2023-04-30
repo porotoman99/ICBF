ec101dff56aa345b4c93e4d1d59979ab3902c45e
Joshua Shroades <pokehax@yahoo.com>
2023-04-30 18:05:04 -0400

    Gloves take damage when attacking

--- src/main/java/icbf/item/custom/GloveItem.java
+++ src/main/java/icbf/item/custom/GloveItem.java
@@ -1,5 +1,8 @@
 package icbf.item.custom;
 
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.ToolMaterial;
 
@@ -7,5 +10,12 @@ public class GloveItem extends ToolItem {
 
     public GloveItem(ToolMaterial material, Settings settings) {
         super(material, settings);
+    }
+
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damage(1, attacker, (e) -> {
+            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+        });
+        return true;
     }
 }
